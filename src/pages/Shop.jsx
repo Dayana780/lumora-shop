4fadf9a9b3e1d70a5e7d22c4b1eb2826c55e7ae6
Dayana <ehsaniandayana@gmail.com>
2026-07-04 21:41:35 +0330

    Day 11 completed 🚀 Connected React app to real API

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
-import products from "../data/products";
 
 function Shop() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+
+        // فقط برای تست Loading
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await res.json();
+
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
   return (
     <div>
       <h1>Shop</h1>
@@ -10,7 +47,7 @@ function Shop() {
         <ProductCard
           key={product.id}
           id={product.id}
-          name={product.name}
+          name={product.title}   // چون FakeStore API از title استفاده می‌کند
           price={product.price}
           image={product.image}
         />
