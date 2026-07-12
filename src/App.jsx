86fac2a3676f435f477d1256757863985a07d520
Dayana <ehsaniandayana@gmail.com>
2026-07-12 22:53:28 +0330

    day-19: add route protection with ProtectedRoute and GuestRoute

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,13 @@ import Wishlist from "./pages/Wishlist";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Dashboard from "./pages/admin/Dashboard";
-
 import MainLayout from "./layouts/MainLayout";
-
-import  AuthProvider  from "./context/AuthContext";
-import  CartProvider  from "./context/CartContext";
-import  WishlistProvider  from "./context/WishlistContext";
+import AuthProvider from "./context/AuthContext";
+import CartProvider from "./context/CartContext";
+import WishlistProvider from "./context/WishlistContext";
 import ProductDetails from "./pages/ProductDetails";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import GuestRoute from "./components/GuestRoute";
 function App() {
   return (
     <AuthProvider>
@@ -26,12 +25,54 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="shop" element={<Shop />} />
               <Route path="product/:id" element={<ProductDetails />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="wishlist" element={<Wishlist />} />
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="admin" element={<Dashboard />} />
+              <Route
+                path="login"
+                element={
+                  <GuestRoute>
+                    <Login />
+                  </GuestRoute>
+                }
+              />
+              <Route
+                path="register"
+                element={
+                  <GuestRoute>
+                    <Register />
+                  </GuestRoute>
+                }
+              />
+              <Route
+                path="wishlist"
+                element={
+                  <ProtectedRoute>
+                    <Wishlist />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="admin"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
             </Route>
           </Routes>
         </WishlistProvider>
